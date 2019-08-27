7549fef5ff021d6f05bcf9704208603dc41eb1af
thecoducer <mayukh5741@gmail.com>
2019-08-27 19:36:00 +0530

    update

--- demoproj/demoapp/views.py
+++ demoproj/demoapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime
+# Day 2
 from demoapp.models import Student
+# Day 3
 from demoapp.form import StudentForm
+# Day 4
 import csv
 from reportlab.pdfgen import canvas
 from reportlab.lib import colors
